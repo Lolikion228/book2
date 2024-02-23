2bbbf03db40dbcbd58b86c1ca3a9196429353d8f
lolikion <st117139@student.spbu.ru>
2024-02-23 18:25:03 +0300

    /

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import numpy as np
 import pandas as pd
+import scipy.interpolate
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder,StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import \
     train_test_split,StratifiedKFold,cross_val_score,\
-    learning_curve,validation_curve
+    learning_curve,validation_curve,GridSearchCV
 import matplotlib.pyplot as plt
-df=pd.read_csv('https://archive.ics.uci.edu/ml/'
-'machine-learning-databases/breast-cancer-wisconsin/wdbc.data',header=None)
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix,make_scorer,f1_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.utils import resample
 
-random_state=1
-le=LabelEncoder()
-X=df.values[:, 2:]
-y=df.values[:, 1]
-y=le.fit_transform(y)
 
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,stratify=y,random_state=random_state)
-pipe_lr=make_pipeline(
-    StandardScaler(),
-    PCA(n_components=2),
-    LogisticRegression(random_state=random_state,
-                       solver="lbfgs",max_iter=10000)
-)
+# df=pd.read_csv('https://archive.ics.uci.edu/ml/'
+# 'machine-learning-databases/breast-cancer-wisconsin/wdbc.data',header=None)
+# random_state=1
+# le=LabelEncoder()
+# X=df.values[:, 2:]
+# y=df.values[:, 1]
+# y=le.fit_transform(y)
+#
+# X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,stratify=y,random_state=random_state)
+# pipe_lr=make_pipeline(
+#     StandardScaler(),
+#     PCA(n_components=2),
+#     LogisticRegression(random_state=random_state,
+#                        solver="lbfgs",max_iter=10000)
+# )
+# pipe_svc=make_pipeline(StandardScaler(),SVC(random_state=1))
 
 # pipe_lr.fit(X_train,y_train)
 # y_pred=pipe_lr.predict(X_test)
@@ -86,6 +94,10 @@ plt.ylim([0.8,1.03])
 plt.show()
 """
 
+
+
+"""  
+###validation_curves
 param_range=[10**exp for exp in range(-8,8)]
 train_scores,test_scores=validation_curve(
     pipe_lr,X_train,y_train,
@@ -126,3 +138,276 @@ plt.xscale('log')
 plt.legend(loc='lower right')
 plt.ylim([0.1,1.0])
 plt.show()
+"""
+
+
+"""
+#вложенная перекрёстная проверка
+#and grid search
+
+param_range=[10**exp for exp in range(-4,5)]
+param_grid=[ {'svc__C':param_range,'svc__kernel':['linear']},
+             {'svc__C':param_range,'svc__kernel':['rbf'], 'svc__gamma':param_range}]
+
+gs=GridSearchCV(estimator=pipe_svc,
+                scoring='accuracy',n_jobs=-1,
+                cv=2,refit=True,
+                param_grid=param_grid)
+
+gs.fit(X_train,y_train)
+print(gs.best_params_)
+print(gs.best_score_)
+print(gs.best_estimator_.score(X_test,y_test))
+
+#вложенная перекрёстная проверка
+print(np.mean(cross_val_score(estimator=gs,X=X_test,y=y_test,cv=5)))
+"""
+
+
+"""
+#confmat
+# pipe_svc.fit(X_train,y_train)
+# y_pred=pipe_svc.predict(X_test)
+# confmat=confusion_matrix(y_test,y_pred)
+# print(confmat)
+# fig,ax=plt.subplots(figsize = (3,3))
+# ax.matshow(confmat,cmap=plt.cm.get_cmap('Blues'),alpha=0.3)
+# for i in range(confmat.shape[0]):
+#     for j in range(confmat.shape[1]):
+#         ax.text(x=j,y=i,
+#                 s=confmat[i,j],
+#                 va='center',ha='center')
+# plt.xlabel('predict')
+# plt.ylabel('true')
+# plt.show()
+
+# param_range=[10**exp for exp in range(-2,3)]
+# param_grid=[ {'svc__C':param_range,'svc__kernel':['linear']},
+#              {'svc__C':param_range,'svc__kernel':['rbf'], 'svc__gamma':param_range}]
+# scorer=make_scorer(f1_score,pos_label=0)
+# gs=GridSearchCV(estimator=pipe_svc,scoring=scorer,n_jobs=-1,cv=10,param_grid=param_grid)
+# gs.fit(X_train,y_train)
+# print(gs.best_score_)
+# print(gs.best_params_)
+"""
+
+
+"""
+#receiver operating characteristic
+from sklearn.metrics import roc_curve,auc
+from scipy import interpolate
+
+pipline_lr=make_pipeline( StandardScaler(),
+                          PCA(n_components=2),
+                          LogisticRegression(solver='lbfgs',
+                                             C=100,
+                                             random_state=1,
+                                            )
+                          )
+X_train2=X_train[:,[4,14]]
+
+cv=list(StratifiedKFold(n_splits=3).split(X_train,y_train))
+
+fig=plt.figure(figsize=(7,5))
+mean_tpr=0.0
+mean_fpr=np.linspace(0,1,100)
+all_tpr=[]
+
+for i,(train,test) in enumerate(cv):
+    pipline_lr.fit(X_train2[train],y_train[train])
+    probas=pipline_lr.predict_proba(X_train2[test])
+
+    fpr,tpr,thresholds= roc_curve(y_train[test],probas[:,1],pos_label=1)
+    print(fpr,tpr)
+
+    mean_tpr+=np.interp(mean_fpr,fpr,tpr)
+
+    mean_tpr[0]=0.0
+    roc_auc=auc(fpr,tpr)
+    plt.plot(fpr,tpr,label='ROC block %d (area= %0.2f)'%(i+1,roc_auc) )
+
+
+plt.plot([0,1],[0,1],label='случайное угадывание',linestyle='--',color=(0.6,0.6,0.6))
+plt.plot([0,0,1],[0,1,1],label='идеально',linestyle=':',color='black')
+
+
+mean_tpr/=len(cv)
+mean_tpr[-1]=1
+mean_auc=auc(mean_fpr,mean_tpr)
+plt.plot(mean_fpr,mean_tpr,'k--',label='средняя ROC (площадь= %0.2f)' % mean_auc, lw=2)
+
+
+plt.legend(loc='lower right')
+
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+
+plt.show()
+"""
+
+
+"""
+#imbalanced data
+X_imb=np.vstack((X[y==0],X[y==1][:40]))
+y_imb=np.hstack((y[y==0],y[y==1][:40]))
+
+print(np.mean(np.zeros(y_imb.shape[0])==y_imb)*100)
+print(f'samples from class 1 initial cnt={y_imb[y_imb==1].shape[0]}')
+X_upsampled,y_upsampled=resample(X_imb[y_imb==1],y_imb[y_imb==1],n_samples=X_imb[y_imb==0].shape[0],
+                                 replace=True)
+print(f'samples from class 1 cnt after resampling={y_upsampled[y_upsampled==1].shape[0]}')
+
+X_bal=np.vstack((X[y==0],X_upsampled))
+y_bal=np.hstack((y[y==0],y_upsampled))
+print(np.mean(np.zeros(y_bal.shape[0])==y_bal)*100)
+"""
+
+
+
+""""
+#ensembles #1
+from scipy.special import comb
+import math
+
+def ensemble_error(n_classifier,error):
+    k_start=math.ceil(n_classifier/2)
+    probs=[comb(n_classifier,k) *
+           (error**k) *
+           ( (1-error)**(n_classifier-k))
+           for k in range(k_start,n_classifier+1)]
+    return sum(probs)
+
+
+n_classifiers=100
+plt.plot(np.linspace(0.0,1.0,100),
+         [ensemble_error(n_classifiers,error) for error in np.linspace(0.0,1.0,100) ],
+         label='ensemble error',linestyle='--'
+         )
+
+plt.plot(np.linspace(0.0,1.0,100),
+         np.linspace(0.0,1.0,100),
+         label='basic error',linestyle=':')
+plt.title(f'Ensemble with {n_classifiers} classifiers error')
+# plt.show()
+
+
+ex=np.array([
+    [0.9,0.1],
+    [0.8,0.2],
+    [0.4,0.6]
+])
+
+mean=np.average(ex,axis=0,weights=[0.2,0.2,0.6])
+print(mean,np.argmax(mean))
+"""
+
+from sklearn.base import BaseEstimator,ClassifierMixin,clone
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import _name_estimators
+import operator
+
+class MajorityVoteClassifier(BaseEstimator,ClassifierMixin):
+    def __init__(self,classifiers,vote='classlabel',weights=None):
+        self.classifiers=classifiers
+        self.named_classeifiers={ k:v for k,v in _name_estimators(classifiers)}
+        self.vote=vote
+        self.weights=weights
+
+    def fit(self,X,y):
+        self.labelenc_=LabelEncoder()
+        self.labelenc_.fit(y)
+        self.classes_=self.labelenc_.classes_
+        self.classifiers_=[]
+        for clf in self.classifiers:
+            fitted_clf=clone(clf).fit(X,self.labelenc_.transform(y))
+            self.classifiers_.append(fitted_clf)
+        return self
+
+    def predict(self,X):
+        if self.vote=='probability':
+            maj_vote=np.argmax( self.predict_proba(X),axis=1)
+        else:
+            preds=np.asarray([clf.predict(X) for clf in self.classifiers_]).T
+            maj_vote=np.apply_along_axis(
+                lambda x:
+                np.argmax(np.bincount(x,weights=self.weights)),
+                axis=1,
+                arr=preds
+            )
+        maj_vote=self.labelenc_.inverse_transform(maj_vote)
+        return maj_vote
+
+    def predict_proba(self,X):
+        probas=np.asarray([clf.predict_proba(X) for clf in self.classifiers_])
+        avg_proba=np.average(probas,axis=0,weights=self.weights)
+        return avg_proba
+
+    def get_params(self,deep=True):
+        if not deep:
+            # return super(MajorityVoteClassifier).get_params()
+            return super(MajorityVoteClassifier, self).get_params(deep=False)
+        else:
+            out=self.named_classeifiers.copy()
+            for name,clf in self.named_classeifiers.items():
+                for key,value in clf.get_params(deep=True).items():
+                    out['%s__%s'%(name,key)]=value
+            return out
+
+from itertools import product
+from sklearn import datasets
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import roc_curve,auc
+iris=datasets.load_iris()
+X,y=iris.data[50:, [1,2]],iris.target[50:]
+le=LabelEncoder()
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.5,stratify=y,random_state=1)
+
+clf1=LogisticRegression(penalty='l2',C=0.001,solver='lbfgs',random_state=1)
+clf2=KNeighborsClassifier(n_neighbors=1,metric='minkowski',p=2)
+clf3=DecisionTreeClassifier(criterion='entropy',max_depth=1)
+
+pipe1=Pipeline([ ['sc',StandardScaler()],['clf',clf1]])
+pipe3=Pipeline([ ['sc',StandardScaler()],['clf',clf3]])
+clf_labels=['LogisticRegression','KNN','DecisionTree']
+
+
+my_clf=MajorityVoteClassifier(classifiers=[pipe1,clf2,pipe3])
+clf_labels+=['MajorityVoting']
+all_clf=[pipe1,clf2,pipe3,my_clf]
+
+for clf,label in zip(all_clf,clf_labels):
+    scores = cross_val_score(clf, X_train, y_train, cv=10, scoring='roc_auc')
+    print("ROC_AUC: %0.2f (+- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+
+
+
+colors=['blue','green','red','orange']
+linestyles=['--','-.','-',':']
+for clf,label,clr,ls in zip(all_clf,clf_labels,colors,linestyles):
+    y_pred=clf.fit(X_train,y_train).predict_proba(X_test)[:,1]
+    fpr,tpr,thresholds=roc_curve(y_test,y_pred,pos_label=2)
+    roc_auc=auc(fpr,tpr)
+    plt.plot(fpr,tpr,
+             color=clr,
+             linestyle=ls,
+             label='%s (auc=%0.2f)' % (label,roc_auc))
+plt.legend(loc='lower right')
+plt.xlabel('FPR')
+plt.ylabel('TPR')
+plt.plot([0,1],[0,1],linestyle='--',color='gray',linewidth=2)
+plt.grid(alpha=0.5)
+plt.tight_layout()
+plt.show()
+plt.close()
+
+
+sc=StandardScaler()
+X_train_std=sc.fit_transform(X_train)
+x_min=X_train_std[:,0].min()-1
+x_max=X_train_std[:,0].max()+1
+y_min=X_train_std[:,1].min()-1
+y_max=X_train_std[:,1].max()+1
+
+xx,yy=np.meshgrid(np.arange(x_min,x_max,0.1),np.arange(y_min,y_max,0.1))
+f,axarr=plt.subplots(nrows=2,ncols=2,sharex='col',sharey='row',figsize=(7,5))
